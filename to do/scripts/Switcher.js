6f9653e4760a494c38d090e65aa8b1e1549a5809
matveevvadim27 <matveev.vadim27@gmail.com>
2025-01-05 21:30:49 +0200

    Сделал переключатель тем

---
+++ to do/scripts/Switcher.js
@@ -0,0 +1,54 @@
+class Switcher {
+  selectors = {
+    root: "[data-js-header]",
+    themeSwitcher: "[data-js-theme-switcher]",
+  };
+
+  stateClasses = {
+    isDark: "dark-theme",
+  };
+
+  constructor() {
+    this.rootElement = document.querySelector(this.selectors.root);
+    if (!this.rootElement) {
+      console.error("Root element not found.");
+      return;
+    }
+
+    this.themeSwitcherElement = this.rootElement.querySelector(
+      this.selectors.themeSwitcher
+    );
+
+    this.initializeTheme();
+    this.bindEvents();
+  }
+
+  initializeTheme() {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      document.body.classList.add(this.stateClasses.isDark);
+      if (this.themeSwitcherElement) {
+        this.themeSwitcherElement.checked = true;
+      }
+    }
+  }
+
+  onSwitcherClick = () => {
+    if (this.themeSwitcherElement?.checked) {
+      document.body.classList.add(this.stateClasses.isDark);
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove(this.stateClasses.isDark);
+      localStorage.setItem("theme", "light");
+    }
+  };
+  bindEvents() {
+    if (this.themeSwitcherElement) {
+      this.themeSwitcherElement.addEventListener(
+        "change",
+        this.onSwitcherClick
+      );
+    }
+  }
+}
+export default Switcher;
